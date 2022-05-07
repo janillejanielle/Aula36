a89a84c72a21978e82ba7d988dadfeb78694b579
janillejanielle <janille.janielle2@gmail.com>
2022-05-07 17:57:18 -0300

    Commit AULA 36

---
+++ filter.js
@@ -0,0 +1,9 @@
+let numeros = [ 1,2,4,5]
+// filtra e retorna um resultado de acordo com a condição, varrendo todos os itens contidos no vetor.
+// Cria uma variável "número" para cada item dentro do vetor "númeos"
+let result = números.filter(
+    function (número){
+        return número>3
+    }
+)
+console.log(result)
